24970c6e3162e0854bc2f2b3dee5ef9255be105c
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2025-04-20 17:54:19 -0400

    Account for message identifier in TelemetryMessage parsing

--- src/limewire/messages/telemetry.py
+++ src/limewire/messages/telemetry.py
@@ -39,7 +39,7 @@ class TelemetryMessage:
     """A class to represent a telemetry message."""
 
     # Class variables
-    HEADER: int = 0x00
+    MSG_ID: int = 0x00
 
     # Instance variables
     board_id: BoardID
@@ -58,15 +58,15 @@ class TelemetryMessage:
         obj = cls()
 
         obj.board_id = BoardID(
-            int.from_bytes(msg_bytes[0:1], byteorder="big", signed=False)
+            int.from_bytes(msg_bytes[1:2], byteorder="big", signed=False)
         )
 
         obj.timestamp = int.from_bytes(
-            msg_bytes[1:9], byteorder="big", signed=False
+            msg_bytes[2:10], byteorder="big", signed=False
         )
 
         obj.values = []
-        for chunk in iterate_chunks(msg_bytes[9:], TELEM_VALUE_SIZE):
+        for chunk in iterate_chunks(msg_bytes[10:], TELEM_VALUE_SIZE):
             data: float = struct.unpack(">f", chunk)[0]
             obj.values.append(data)
 
@@ -93,7 +93,7 @@ class TelemetryMessage:
 
     def __bytes__(self) -> bytes:
         msg_bytes = (
-            self.HEADER.to_bytes(1)
+            self.MSG_ID.to_bytes(1)
             + self.board_id.to_bytes(1)
             + self.timestamp.to_bytes(8)
         )
