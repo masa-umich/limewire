078f8cb9280dfa21c2e7c62cf3b4df623d2ac93f
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2025-04-25 16:11:00 -0400

    Update fc_simulator to respond to valve command messages

--- src/fc_simulator/fc_simulator.py
+++ src/fc_simulator/fc_simulator.py
@@ -4,7 +4,12 @@ from functools import partial
 
 import synnax as sy
 
-from limewire.messages import Board, TelemetryMessage
+from limewire.messages import (
+    Board,
+    TelemetryMessage,
+    ValveCommandMessage,
+    ValveStateMessage,
+)
 
 
 async def generate_telemetry_data(
@@ -13,7 +18,6 @@ async def generate_telemetry_data(
     """Send randomly generated telemetry data to Limewire."""
 
     start_time = asyncio.get_running_loop().time()
-    synnax_start_time = None
 
     boards = [
         Board.FC,
@@ -31,9 +35,6 @@ async def generate_telemetry_data(
             values = [i * random.uniform(0, 1) for i in range(board.num_values)]
 
             timestamp = sy.TimeStamp.now()
-            if synnax_start_time is None:
-                synnax_start_time = timestamp
-
             msg = TelemetryMessage(board, timestamp, values)
             msg_bytes = bytes(msg)
 
@@ -50,16 +51,39 @@ async def generate_telemetry_data(
         loop_elapsed_time = asyncio.get_running_loop().time() - loop_start_time
         await asyncio.sleep(max(0, 1 / DATA_RATE - loop_elapsed_time))
 
+    actual_run_time = asyncio.get_running_loop().time() - start_time
+
     print(f"Connection with {addr} closed.")
     writer.close()
     await writer.wait_closed()
     print(
-        f"Sent {values_sent} telemetry values in {run_time} sec ({values_sent / run_time:.2f} values/sec)"
+        f"Sent {values_sent} telemetry values in {actual_run_time} sec ({values_sent / actual_run_time:.2f} values/sec)"
     )
 
 
+async def handle_valve_commands(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+):
+    while True:
+        msg_length = await reader.read(1)
+        if not msg_length:
+            break
+
+        msg_length = int.from_bytes(msg_length)
+        msg_bytes = await reader.readexactly(msg_length)
+        if not msg_bytes:
+            break
+
+        msg_id = int.from_bytes(msg_bytes[0:1])
+        if msg_id == ValveCommandMessage.MSG_ID:
+            cmd_msg = ValveCommandMessage.from_bytes(msg_bytes)
+            state_msg = ValveStateMessage(cmd_msg.valve, cmd_msg.state)
+            writer.write(bytes(state_msg))
+            await writer.drain()
+
+
 async def handle_client(
-    _: asyncio.StreamReader,
+    reader: asyncio.StreamReader,
     writer: asyncio.StreamWriter,
     run_time: float,
 ) -> None:
@@ -69,8 +93,10 @@ async def handle_client(
     telemetry_task = asyncio.create_task(
         generate_telemetry_data(addr, writer, run_time)
     )
+    valve_task = asyncio.create_task(handle_valve_commands(reader, writer))
 
     await telemetry_task
+    await valve_task
 
 
 async def run_server(ip_addr: str, port: int, run_time: float) -> None:
