fced700fe49b54ba496107ef54302a5ac95d2a13
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2025-09-21 18:02:17 -0400

    Fix error handling and upgrade to Synnax v0.45.0

--- src/limewire/__main__.py
+++ src/limewire/__main__.py
@@ -13,12 +13,10 @@ from limewire.util import SocketAddress
 def main(fc_address: tuple[str, int]):
     """Run Limewire."""
 
+    limewire = Limewire()
+
     try:
-        limewire = Limewire()
         asyncio.run(limewire.start(fc_address))  # pyright: ignore[reportPrivateLocalImportUsage]
-    except KeyboardInterrupt:
-        print("\nCtrl+C recieved.")
-        sys.exit(0)
     except ConnectionRefusedError as err:
         print_limewire_error(err)
 
