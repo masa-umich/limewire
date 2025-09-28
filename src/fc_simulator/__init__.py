b37f734d3ff5b0e83f68d372b286b1a0d2f29a4f
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2025-09-28 16:18:59 -0400

    Make fc_simulator into project script

--- src/fc_simulator/__init__.py
+++ src/fc_simulator/__init__.py
@@ -1,2 +1,26 @@
+import asyncio
+import click
+
+from limewire.util import SocketAddress
+
 from .fc_simulator import FCSimulator
 from . import errors
+
+@click.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument("fc_address", type=SocketAddress())
+@click.option(
+    "-r",
+    "--runtime",
+    type=float,
+    default=10,
+    help="Amount of time to generate telemetry packets per client (sec).",
+)
+def main(fc_address: tuple[str, int], runtime: float):
+    """Run the FC simulator."""
+
+    try:
+        fc_simulator = FCSimulator()
+        asyncio.run(fc_simulator.run(*fc_address, runtime))
+    except KeyboardInterrupt:
+        print("\nCtrl+C received.")
+        exit(0)
