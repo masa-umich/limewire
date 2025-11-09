1408f3806cadbb924ba842dfec8e735fdac688b9
ryan Wei <rywbook06@gmail.com>
2025-11-09 14:58:44 -0500

    Add new heartbeat message class

---
+++ src/limewire/messages/heartbeat.py
@@ -0,0 +1,15 @@
+import struct
+
+
+class HeartbeatMessage:
+    """A class to represent heartbeat messages"""
+
+    MSG_ID: int = 0x03
+
+    def __bytes__(self):
+        msg_bytes = self.MSG_ID.to_bytes(1)
+
+        return msg_bytes
+
+    def __repr__(self):
+        return "HeartbeatMessage()"
