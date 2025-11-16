6cbdae9bd65214c85a9260e6408cb5a212a9bc4d
ryan Wei <rywbook06@gmail.com>
2025-11-16 16:21:44 -0500

    Setup logging in __main__

--- src/limewire/__main__.py
+++ src/limewire/__main__.py
@@ -1,59 +1,11 @@
 import asyncio
-import logging
-import logging.handlers
-import os
-from datetime import datetime
 
 import click
-from platformdirs import user_log_dir
 
 from limewire import Limewire
 from limewire.errors import print_limewire_error
+from limewire.logging import setup_logging
 from limewire.util import SocketAddress
-
-
-class ErrorCodeFormatter(logging.Formatter):
-    def format(self, record):
-        # Ensure error_code exists
-        if not hasattr(record, "error_code"):
-            record.error_code = "Unknown"
-
-        return super().format(record)
-
-
-def setup_logging(verbosity: str):
-    # TODO: confirm this
-    log_dir = user_log_dir("limewire", "masa")
-    os.makedirs(log_dir, exist_ok=True)
-    run_ts = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    log_path = os.path.join(log_dir, f"limewire-{run_ts}.log")
-
-    logging_level = (
-        logging.DEBUG
-        if verbosity == "debug"
-        else logging.ERROR
-        if verbosity == "sparse"
-        else logging.INFO
-    )
-
-    log_format = "%(asctime)s.%(msecs)03d %(error_code)s %(message)s"
-    log_dateFmt = "%Y-%m-%dT%H:%M:%S"
-    formatter = ErrorCodeFormatter(log_format, datefmt=log_dateFmt)
-
-    file_handler = logging.FileHandler(log_path, encoding="utf-8")
-    file_handler.setFormatter(formatter)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-
-    # TODO: Change address once port is chosen
-    syslog_handler = logging.handlers.SysLogHandler(address=("localhost", 1234))
-    syslog_handler.setFormatter(formatter)
-
-    logging.basicConfig(
-        level=logging_level,
-        handlers=[file_handler, stream_handler, syslog_handler],
-    )
 
 
 @click.command(context_settings={"help_option_names": ["--help", "-h"]})
