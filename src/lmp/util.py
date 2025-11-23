146b5b3103d5bf4bc2910cac38d1339f7df747d6
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2025-11-23 16:43:15 -0500

    Update number of channels for each board

--- src/lmp/util.py
+++ src/lmp/util.py
@@ -32,11 +32,11 @@ class Board(Enum):
     def num_values(self) -> int:
         """The number of telemetry values for this board."""
         NUM_VALUES = {
-            Board.FC: 39,
-            Board.BB1: 52,
-            Board.BB2: 52,
-            Board.BB3: 52,
-            Board.FR: 14,
+            Board.FC: 40,
+            Board.BB1: 53,
+            Board.BB2: 53,
+            Board.BB3: 53,
+            Board.FR: 15,
         }
         return NUM_VALUES[self]
 
