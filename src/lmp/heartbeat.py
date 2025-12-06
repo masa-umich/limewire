5cf16081e7dc292078ebcc7725b66b4c6aedff01
ryan Wei <rywbook06@gmail.com>
2025-12-05 21:42:33 -0500

    Add message length to heartbeat

--- src/lmp/heartbeat.py
+++ src/lmp/heartbeat.py
@@ -4,8 +4,8 @@ class HeartbeatMessage:
     MSG_ID: int = 0x03
 
     def __bytes__(self):
-        msg_bytes = self.MSG_ID.to_bytes(1)
-
+        msg_length = 1
+        msg_bytes = msg_length.to_bytes(1) + self.MSG_ID.to_bytes(1)
         return msg_bytes
 
     def __repr__(self):
