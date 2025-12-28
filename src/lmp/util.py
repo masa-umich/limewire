19e0ed80f0afcb512949f3764b1473391e34b8bf
Felix F-B <felixfbpuppy@gmail.com>
2025-12-27 21:49:39 -0500

    Removed duplicate property & reverted unintentional versioning constraint on synnax

--- src/lmp/util.py
+++ src/lmp/util.py
@@ -33,17 +33,6 @@ class Board(Enum):
     FR = 4
     
     @property
-    def pretty_name(self) -> str:
-        PRETTY_NAMES = {
-            Board.FC: "Flight Computer",
-            Board.BB1: "Bay Board 1",
-            Board.BB2: "Bay Board 2",
-            Board.BB3: "Bay Board 3",
-            Board.FR: "Flight Recorder",
-        }
-        return PRETTY_NAMES[self]
-    
-    @property
     def num_values(self) -> int:
         """The number of telemetry values for this board."""
         NUM_VALUES = {
