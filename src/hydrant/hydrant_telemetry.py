0fb15621b655a98d8980c605f35c76ad1c19286a
Felix F-B <felixfbpuppy@gmail.com>
2026-01-14 22:09:02 -0500

    Please work

--- src/hydrant/hydrant_telemetry.py
+++ src/hydrant/hydrant_telemetry.py
@@ -1,6 +1,8 @@
 import asyncio
 import datetime
 from enum import Enum
+import socket
+import sys
 
 from loguru import logger
 from nicegui import Client, ui
@@ -200,14 +202,19 @@ class TelemetryListener:
         while True:
             try:
                 loop = asyncio.get_event_loop()
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+                if sys.platform != "win32":
+                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                sock.bind(("0.0.0.0", TELEM_PORT))
                 (
                     self.transport,
                     self.handler,
                 ) = await loop.create_datagram_endpoint(
                     self.create_protocol,
-                    ("0.0.0.0", TELEM_PORT),
-                    allow_broadcast=True,
-                    reuse_address=True,
+                    sock=sock
                 )
             except Exception as err:
                 logger.error(f"Error opening telemetry listener: {str(err)}")
