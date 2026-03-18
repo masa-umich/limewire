c9054c6cbe2a9e9920c6d58e43abcbd95f8b8165
ryan Wei <rywbook06@gmail.com>
2026-03-17 20:01:46 -0400

    remove comments

--- src/utils/ntp_sync.py
+++ src/utils/ntp_sync.py
@@ -1,6 +1,3 @@
-# from scapy.all import IP, UDP, send
-# from scapy.error import Scapy_Exception
-# from scapy.layers.ntp import NTPHeader
 import ipaddress
 import sys
 
@@ -8,21 +5,6 @@ from loguru import logger
 from scapy.all import IP, UDP, send
 from scapy.interfaces import get_working_ifaces
 from scapy.layers.ntp import NTPHeader
-
-
-# def send_ntp_sync(ip_addr: str, port: int, logger=None):
-#     if logger is not None:
-#         logger.info("Sending NTP sync.")
-
-#     ntp_packet = NTPHeader(mode=5)
-#     # packet = IP(dst="141.212.192.255") / UDP(dport=123) / ntp_packet
-#     packet = IP(dst=ip_addr) / UDP(dport=port) / ntp_packet
-
-#     try:
-#         send(packet)
-#     except Scapy_Exception:
-#         if logger is not None:
-#             logger.warning("NTP sync failed.")
 
 
 def send_all():
